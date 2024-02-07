a75b1c3397c759c13f718255dfe29d63f46c2481
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2024-02-07 17:51:33 -0500

    New header
    - New header for mobile.
    - Restyled header for desktop.
    - Add icons for header.
    - Add accent red color.
    - Remove `scroll.js`.

--- assets/js/scroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
-  if (window.scrollY > 0) {
-    header.setAttribute("data-scrolled", "true");
-  } else {
-    header.removeAttribute("data-scrolled");
-  }
-});
